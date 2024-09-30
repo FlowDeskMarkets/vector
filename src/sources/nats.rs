442aaec339f058333082dee0f74caa819eaa85fe
benjaminawd-flowdesk <benjamin.dornel@flowdesk.co>
2024-09-30 15:36:52 +0800

    feat: add multiple NATS url

--- src/sources/nats.rs
+++ src/sources/nats.rs
@@ -185,7 +185,18 @@ impl SourceConfig for NatsSourceConfig {
 impl NatsSourceConfig {
     async fn connect(&self) -> Result<async_nats::Client, BuildError> {
         let options: async_nats::ConnectOptions = self.try_into().context(ConfigSnafu)?;
-        options.connect(&self.url).await.context(ConnectSnafu)
+
+        let server_addrs = self.parse_server_addresses()?;
+        options.connect(server_addrs).await.context(ConnectSnafu)
+    }
+
+    fn parse_server_addresses(&self) -> Result<Vec<async_nats::ServerAddr>, BuildError> {
+        self.url.split(',')
+            .map(|url| url.parse::<async_nats::ServerAddr>()
+                .map_err(|_| BuildError::Connect {
+                    source: async_nats::ConnectErrorKind::ServerParse.into(),
+                }))
+            .collect()
     }
 }
 
@@ -846,4 +857,56 @@ mod integration_tests {
             r
         );
     }
+
+    #[tokio::test]
+    async fn nats_multiple_urls_valid() {
+        let subject = format!("test-{}", random_string(10));
+        
+        let conf = NatsSourceConfig {
+            connection_name: "".to_owned(),
+            subject: subject.clone(),
+            url: "nats://nats:4222,nats://demo.nats.io:4222".to_string(),
+            queue: None,
+            framing: default_framing_message_based(),
+            decoding: default_decoding(),
+            tls: None,
+            auth: None,
+            log_namespace: None,
+            subject_key_field: default_subject_key_field(),
+            ..Default::default()
+        };
+    
+        let r = publish_and_check(conf).await;
+        assert!(
+            r.is_ok(),
+            "publish_and_check failed for multiple URLs, expected Ok(()), got: {:?}",
+            r
+        );
+    }
+
+    #[tokio::test]
+    async fn nats_multiple_urls_invalid() {
+        let subject = format!("test-{}", random_string(10));
+        
+        let conf = NatsSourceConfig {
+            connection_name: "".to_owned(),
+            subject: subject.clone(),
+            url: "http://invalid-url,nats://:invalid@localhost:4222".to_string(),
+            queue: None,
+            framing: default_framing_message_based(),
+            decoding: default_decoding(),
+            tls: None,
+            auth: None,
+            log_namespace: None,
+            subject_key_field: default_subject_key_field(),
+            ..Default::default()
+        };
+    
+        let r = publish_and_check(conf).await;
+        assert!(
+            matches!(r, Err(BuildError::Connect { .. })),
+            "publish_and_check failed for bad URLs, expected BuildError::Connect, got: {:?}",
+            r
+        );
+    }    
 }
